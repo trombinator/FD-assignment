5ea363b4d79508e3886440812ccd9cb662bf5a30
borisenglebert <englebertboris@hotmail.com>
2019-02-17 20:10:21 +0100

    Update AE3212 cross-section.py

--- AE3212 cross-section.py
+++ AE3212 cross-section.py
@@ -1,5 +1,5 @@
-#---------------CROSS SECTION DEFINITION------------#
-from math import*
+# ---------------CROSS SECTION DEFINITION------------#
+from math import *
 from matplotlib import pyplot as plt
 
 ##NOTE: coordinate system is defined with origin at the trailing edge and
@@ -9,79 +9,81 @@ from matplotlib import pyplot as plt
 ##both half a stiffener pitch away from the TE. Stiffeners are equally spaced
 ##over cross section.
 
-#------STARTING VALUES-----------
-c = 0.515 #aileron chord
-h = 0.248 #aileron height
-r = h/2 #leading edge section radius
-le = sqrt((c-r)**2+r**2) #length of linear section
-circ = pi*r + 2*le #circumference of cross section
-phi = circ/11 #stiffener spacing
+# ------STARTING VALUES-----------
+c = 0.515  # aileron chord
+h = 0.248  # aileron height
+r = h / 2  # leading edge section radius
+le = sqrt((c - r) ** 2 + r ** 2)  # length of linear section
+circ = pi * r + 2 * le  # circumference of cross section
+phi = circ / 11  # stiffener spacing
 
-ypos = [] #y' position of stiffener
-zpos = [] #z' position of stiffener
-nstiff = [] #Stiffener numbering
+ypos = []  # y' position of stiffener
+zpos = []  # z' position of stiffener
+nstiff = []  # Stiffener numbering
 n = 0
 
-while n<=10: #Collecting coordinate data of stiffeners
-    if n==0:
-        dely = (phi/2)*(r/le)
-        delz = (phi/2)*((c-r)/le)
+while n <= 10:  # Collecting coordinate data of stiffeners
+    if n == 0:
+        dely = (phi / 2) * (r / le)
+        delz = (phi / 2) * ((c - r) / le)
         ypos.append(dely)
         zpos.append(delz)
-        nstiff.append(n+1)
-        
+        nstiff.append(n + 1)
 
-    elif n>0 and ypos[n-1]>0:
-        if (ypos[n-1]+(phi*r)/le)<r:
-            dely = (phi*r)/le
-            delz = (phi*(c-r))/le
-            ynew = dely + ypos[n-1]
-            znew = delz + zpos[n-1]
+
+    elif n > 0 and ypos[n - 1] > 0:
+        if (ypos[n - 1] + (phi * r) / le) < r:
+            dely = (phi * r) / le
+            delz = (phi * (c - r)) / le
+            ynew = dely + ypos[n - 1]
+            znew = delz + zpos[n - 1]
             ypos.append(ynew)
             zpos.append(znew)
-            nstiff.append(n+1)
-            
-        elif (ypos[n-1]+(phi*r)/le)>r and zpos[n-1]<(c-r):
-            eta = phi - (le*(r-ypos[n-1]))/r
-            ynew = r*cos(eta/r)
-            znew= ((c-r)+r*sin(eta/r))
+            nstiff.append(n + 1)
+
+        elif (ypos[n - 1] + (phi * r) / le) > r and zpos[n - 1] < (c - r):
+            eta = phi - (le * (r - ypos[n - 1])) / r
+            ynew = r * cos(eta / r)
+            znew = ((c - r) + r * sin(eta / r))
             ypos.append(ynew)
             zpos.append(znew)
-            nstiff.append(n+1)
+            nstiff.append(n + 1)
 
-        elif zpos[n-1]>(c-r):
+        elif zpos[n - 1] > (c - r):
             ypos.append(0)
             zpos.append(c)
+            nstiff.append(n + 1)
+
+    elif n > 0 and ypos[n - 1] <= 0:
+        if ypos[n-1]==0:
+            ynew = -r*sin(phi/r)
+            znew = r*cos(phi/r)+(c-r)
+            ypos.append(ynew)
+            zpos.append(znew)
             nstiff.append(n+1)
 
-    elif n>0 and ypos[n-1]<=0:
-        if ypos[n-1]==0:
-            dely = -(phi/2)*(r/le)
-            delz = (phi/2)*((c-r)/le)
-            ypos.append(dely)
-            zpos.append(delz)
-            nstiff.append(n+1)
-        
-        if (ypos[n-1]-(phi*r)/le)>-r and ypos[n-1]!=0:
-            dely = -(phi*r)/le
-            delz = (phi*(c-r))/le
-            ynew = dely + ypos[n-1]
-            znew = delz + zpos[n-1]
+        elif ypos[n-1]>-r and zpos[n-1]>(c-r):
+            eta = r*acos(-ypos[n-1]/r)
+            ynew = -r + (phi-eta)*(r/le)
+            znew = (c-r)-(phi-eta)*((c-r)/le)
             ypos.append(ynew)
             zpos.append(znew)
             nstiff.append(n+1)
-            
-        elif (ypos[n-1]-(phi*r)/le)<-r and zpos[n-1]<(c-r):
-            eta = phi - (le*(r+ypos[n-1]))/r
-            ynew = -r*cos(eta/r)
-            znew= ((c-r)+r*sin(eta/r))
+
+        elif ypos[n - 1] > -r and zpos[n - 1] < (c - r):
+            ynew = ypos[n-1]+(phi*r/le)
+            znew = zpos[n-1]-(phi*(c-r)/le)
             ypos.append(ynew)
             zpos.append(znew)
             nstiff.append(n+1)
-    n+=1
+
+    n += 1
 
 ##return nstiff, zpos, ypos
-plt.plot(zpos, ypos, "ro")
+print(ypos)
+print(zpos)
+print(nstiff)
+plt.plot(zpos, ypos, "-ro")
 plt.gca().invert_xaxis()
 plt.axis("equal")
 plt.grid(linestyle='-', linewidth='0.5')
